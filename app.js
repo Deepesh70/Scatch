27e632eb0ccc635af85f7d1141b9dacf8142d580
Deepesh70 <deepeshdangi700@gmail.com>
2025-09-22 22:58:02 +0530

    created routes - owner, product, user

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
 const app = express();
-
 const cookieParser = require('cookie-parser');
 const path = require("path");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/users.Router");
+const productsRouter = require("./routes/productRouter");
+
+const db = require("./config/mongoose-connection");
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
@@ -14,6 +19,10 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.send("hey");
 });
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(3000);
 console.log("Server started at port http://localhost:3000");
